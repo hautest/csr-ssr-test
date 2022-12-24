0c654d45694389a7d3268c4ebcf44a98cd3fefdb
LeeSeungHun <dos07008@naver.com>
2022-12-24 16:16:16 +0900

    csr세팅

---
+++ pages/index.tsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+import type { NextPage } from "next";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+interface Users {
+  avatar: string;
+  createdAt: Date;
+  id: string;
+  name: string;
+}
+
+const Home: NextPage = () => {
+  const [user, setUser] = useState<Users[]>();
+  useEffect(() => {
+    const getUser = async () => {
+      const result = await axios.get(
+        "https://63a6a469f8f3f6d4ab0f5e08.mockapi.io/api/users"
+      );
+      setUser(result.data);
+    };
+    getUser();
+  }, []);
+
+  return (
+    <div>
+      <h1>User list</h1>
+      {user?.map((item) => (
+        <div key={item.id}>
+          <Image
+            width={40}
+            height={40}
+            src={item.avatar}
+            alt={`${item.name} img`}
+          />
+          <span>{item.name}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Home;
