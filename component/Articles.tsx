9f9a915f5ef48ad19db237e69627f0a6163dda7b
LeeSeungHun <dos07008@naver.com>
2022-12-26 01:22:17 +0900

    ssr 병합

--- component/Articles.tsx
+++ component/Articles.tsx
@@ -1,23 +1,10 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import { Article } from "../pages";
 
 interface ArticlesProps {
   articles: Article[];
 }
 
-export const Articles = () => {
-  const [articles, setArticles] = useState<Article[]>();
-  useEffect(() => {
-    const getArticles = async () => {
-      const result = await axios.get(
-        "https://63a6a469f8f3f6d4ab0f5e08.mockapi.io/api/article"
-      );
-      setArticles(result.data);
-    };
-    getArticles();
-  }, []);
-
+export const Articles = ({ articles }: ArticlesProps) => {
   return (
     <>
       <h2>Articles List</h2>
