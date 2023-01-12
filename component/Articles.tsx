98b03f5386cabc55a1ccc60de0a65d2ecc450ce6
LeeSeungHun <dos07008@naver.com>
2023-01-12 21:37:21 +0900

    이미지 삭제, h1, h2 테그 삭제

--- component/Articles.tsx
+++ component/Articles.tsx
@@ -8,7 +8,6 @@ interface ArticlesProps {
 export const Articles = ({ articles }: ArticlesProps) => {
   return (
     <>
-      <h2>Articles List</h2>
       {articles.map((item) => (
         <li key={item.id}>
           <a href={item.url}>{item.title}</a>
